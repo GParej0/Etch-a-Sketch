ab007d20f3da82f15356a631e68cd07e2e1c1264
GParej0 <195317371+GParej0@users.noreply.github.com>
2026-02-24 16:17:24 +0100

    Adding button and color
    Adding the button to be able to select the number of squares and giving them some ramdon color

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Etch-a-Sketch
+
+Boton que define el tamaño(JS)
+
+bontón con escucha(click) con un promt.
+recopilar información del tamaño y pasarlo a la funcion createDivs
+
+funcion createDivs
+borrar grid anterior container = null;
+si no es igual a un num alert, no es un num y menor que 100
+si cumple los requisitos 
+total de cuadros = valor*valor
+y funcion for( i < total de cuadrados; i++){
+        const div = document.createElement("div");
+        container.appendChild(div);
+        div.addEventListener("mouseenter", ()=>{
+            div.style.backgroundColor = "pink";
+        })
+    }
+
+
+createDivs();
--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 const container = document.querySelector(".container");
-function createDivs(){
-    for(let i = 0; i < 256; i++){
+const button = document.querySelector("button");
+button.addEventListener("click",() => {
+    let squares = parseInt(prompt("Cuantos cuadrados por lado?"));
+    function definingSize(squares){
+        while(isNaN(squares) || squares > 100){
+            if(isNaN(squares)){
+                alert("Eso no es un número");
+            } else if (squares > 100){
+                alert("Ese número es muy alto");
+            
+        } squares = prompt("Intenta de nuevo. ¿Cuántos cuadrados por lado?")
+    } return squares
+}
+let size = definingSize(squares);
+container.innerHTML = "";
+createDivs(size);
+});
+function createDivs(size){
+    let totalSize = size*size;
+    for(let i = 0; i < totalSize; i++){
         const div = document.createElement("div");
         container.appendChild(div);
+        div.style.width = (500/size) + "px";
+        div.style.height = (500/size) + "px";
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
         div.addEventListener("mouseenter", ()=>{
-            div.style.backgroundColor = "pink";
-        })
-    }
+            div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }
+    
+)}
 }
-
-createDivs();
